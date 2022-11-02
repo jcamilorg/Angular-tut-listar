b6e303083eff502ccb0c47b71d36ec60ae659406
jcamilorg <juancamilorg64@gmail.com>
2022-11-02 07:33:51 -0500

    Detalle del libro

---
+++ src/app/book/book.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { BookDetail } from './book-detail';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private apiUrl: string = environment.baseUrl + 'books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<BookDetail[]> {
+    return this.http.get<BookDetail[]>(this.apiUrl);
+  }
+}
